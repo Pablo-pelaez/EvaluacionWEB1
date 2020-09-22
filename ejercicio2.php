31f19ad574cd7eaada58d8b8621b4033fc64c446
Juan Pablo <71094683+Pablo-pelaez@users.noreply.github.com>
2020-09-22 18:00:15 -0500

    Resolución ejrcicio2

--- ejercicio2.php
+++ ejercicio2.php
@@ -6,12 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taller de PHP</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
 </head>
 
 <body>
-
     <header>
-
         <div class="jumbotron">
             <div class="container">
                 <h1>DESARROLLO DE PHP</h1>
@@ -26,83 +25,50 @@
                 </div>
             </div>
         </div>
-
     </header>
 
     <main>
-
         <div class="container my-5">
-            <form action="index.php" method="POST">
+        </div>
+
+        <div class="container">
+            <form action="ejercicio2.php" method="$_POST">
                 <div class="form-group">
-                    <h4 class="text-center my-4">CALCULADORA BÁSICA</h4>
-                    <label for="exampleInputPassword1"><b>Número 1</b></label>
-                    <input type="number" class="form-control" name="numero1">
+                    <h2 class="text-center">Prevención de Riesgo de IMC</h2>
+                    <label for="example"><b>Ingresa tu peso</b></label>
+                    <input type="number" class="form-control" name="peso">
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1"><b>Número 2</b></label>
-                    <input type="number" class="form-control" name="numero2">
-                </div>
-                <div>
-                    <select name="operador" class="mb-3">
-                        <option value="1">Sumar</option>
-                        <option value="2">Restar</option>
-                        <option value="3">Multiplicar</option>
-                        <option value="4">Dividir</option>
-                    </select>
+                    <label for="example"><b>Ingresa tu altura</b></label>
+                    <input type="number" class="form-control" name="altura">
                 </div>
                 <button type="submit" class="btn btn-info" name="btnCalcular" value="cj">Calcular</button>
+
+
             </form>
 
-            <?php
-            if (isset($_POST["btnCalcular"])) :
-            ?>
+            <?php if (isset($_POST["btnCalcular"])): ?>
 
-                <h4 class="mt-4">
-                    <?php
-                    $numero1 = $_POST["numero1"];
-                    $numero2 = $_POST["numero2"];
-                    $operador = $_POST["operador"];
-
-                    if ($numero1 !== "" && $numero2 !== "") {
-                        switch ($operador) {
-                            case 1:
-                                $resultado = $numero1 + $numero2;
-                                echo ("El resultado es: " . $resultado);
-                                break;
-
-                            case 2:
-                                $resultado = $numero1 - $numero2;
-                                echo ("El resultado es: " . $resultado);
-                                break;
-
-                            case 3:
-                                $resultado = $numero1 * $numero2;
-                                echo ("El resultado es: " . $resultado);
-                                break;
-
-                            case 4:
-                                $resultado = $numero1 / $numero2;
-                                echo ("El resultado es: " . $resultado);
-                                break;
-                        }
-                    } else {
-                        echo ("Debes ingresar los dos valores");
-                    }
-                    ?>
-                </h4>
+                <?php
+                // $peso = $_POST["peso"];
+                // $altura = $_POST["altura"];
+                echo("subnormal");
+                ?>
 
             <?php endif; ?>
 
         </div>
-
     </main>
 
     <footer>
+
     </footer>
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
